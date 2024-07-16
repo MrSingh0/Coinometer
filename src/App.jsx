5346932061c9534306422338ec1f7e32620270d7
MrSingh0 <pksingh9016@gmail.com>
2024-07-16 10:04:30 +0530

    Normalize line endings

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,81 @@
 import { Dashboard } from '@mui/icons-material'
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import HomePage from './Pages/Home'
 import DashboardPage from './Pages/Dashboard'
+import Compare from './Pages/Compare'
 import CoinPage from './Pages/Coin'
-import ComparePage from './Pages/ComparePage'
+
+import { createTheme, ThemeProvider } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Watchlist from "./Pages/Watchlist";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { useEffect } from "react";
+
 
 function App() {
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#3a80e9",
+      },
+    },
+  });
+
+  var cursor;
+  var cursorPointer;
+
+  useEffect(() => {
+    cursor = document.getElementById("cursor");
+    cursorPointer = document.getElementById("cursor-pointer");
+
+    document.body.addEventListener("mousemove", function (e) {
+      return (
+        (cursor.style.left = e.clientX + "px"),
+        (cursor.style.top = e.clientY + "px"),
+        (cursorPointer.style.left = e.clientX + "px"),
+        (cursorPointer.style.top = e.clientY + "px")
+      );
+    });
+
+    document.body.addEventListener("mousedown", function (e) {
+      return (
+        (cursor.style.height = "0.5rem"),
+        (cursor.style.width = "0.5rem"),
+        (cursorPointer.style.height = "3rem"),
+        (cursorPointer.style.width = "3rem")
+      );
+    });
+
+    document.body.addEventListener("mouseup", function (e) {
+      return (
+        (cursor.style.height = "0.3rem"),
+        (cursor.style.width = "0.3rem"),
+        (cursorPointer.style.height = "2rem"),
+        (cursorPointer.style.width = "2rem")
+      );
+    });
+  }, []);
+
   return (
-    <div className='App'>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/coin/:id" element={<CoinPage />} />
-          <Route path="/compare
-          " element={<ComparePage />} /> 
-          {/*<Route path="/watchlist" element={<WatchlistPage />} /> */}
-        </Routes>
-      </BrowserRouter>
+    <div className="App">
+      <div className="cursor" id="cursor" />
+      <div className="cursor-pointer" id="cursor-pointer" />
+      <ToastContainer />
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/dashboard" element={<DashboardPage />} />
+            <Route path="/coin/:id" element={<CoinPage />} />
+            <Route path="/compare" element={<Compare />} />
+            <Route path="/watchlist" element={<Watchlist />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Pages/Watchlist.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import Button from "../Components/Common/Button/Button";
+import Header from "../Components/Common/Header/Header";
+import TabsComponent from "../Components/Dashboard/Tabs/TabsComponent";
+import { get100Coins } from "../Functions/get100coins";
+
+function Watchlist() {
+  const watchlist = JSON.parse(localStorage.getItem("watchlist"));
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    if (watchlist) {
+      getData();
+    }
+  }, []);
+
+  const getData = async () => {
+    const allCoins = await get100Coins();
+    if (allCoins) {
+      setCoins(allCoins.filter((coin) => watchlist.includes(coin.id)));
+    }
+  };
+
+  return (
+    <div>
+      <Header />
+      {watchlist?.length > 0 ? (
+        <TabsComponent coins={coins} />
+      ) : (
+        <div>
+          <h1 style={{ textAlign: "center" }}>
+            Sorry, No Items In The Watchlist.
+          </h1>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              margin: "2rem",
+            }}
+          >
+            <a href="/dashboard">
+              <Button text="Dashboard" />
+            </a>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Watchlist;
